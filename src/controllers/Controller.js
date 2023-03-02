a01dc94eb8f81a15b2a803304b0d1ea514650099
Ellinor Henriksson <eh224kr@student.lnu.se>
2023-03-02 14:11:10 +0100

    Refactor oauth/callback into a service class

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -48,41 +48,24 @@ export class Controller {
   // Redirect url for oauth
   async oauthCallback (req, res, next) {
     if (req.query.state === req.session.state) {
-      const code = req.query.code
-      console.log('Code: ', code)
-      // Request access token
       try {
-        const response = await fetch(`https://gitlab.lnu.se/oauth/token?client_id=${process.env.APP_ID}&client_secret=${process.env.APP_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=${process.env.CALLBACK}`, { method: 'POST' })
+        const { accessToken, user } = await this.#service.requestAccessToken(req.query.code)
 
-        if (!response.ok) {
-          console.log(response)
-          throw createError(response.status, response.statusText)
-        }
-
-        const data = await response.json()
-
-        // Regenerate session
         req.session.regenerate(error => {
           if (error) {
             throw new Error('Failed to regenerate session.')
           }
         })
 
-        // Store access token and user info in session store
-        req.session.accessToken = data.access_token
-        const payload = jwt.decode(data.id_token, { json: true })
-        req.session.user = {
-          id: payload.sub,
-          email: payload.email,
-          groups: payload.groups_direct
-        }
+        req.session.accessToken = accessToken
+        req.session.user = user
 
         res.redirect('/')
       } catch (error) {
         next(error)
       }
-    } else {
-      next(createError(400))
     }
+
+    next(createError(400))
   }
 }
--- src/services/GitLabService.js
+++ src/services/GitLabService.js
@@ -1,5 +1,32 @@
 import { ServiceBase } from './ServiceBase.js'
+import fetch from 'node-fetch'
+import createError from 'http-errors'
+import jwt from 'jsonwebtoken'
 
 export class GitLabService extends ServiceBase {
-  
+  /**
+   * Requests an access token from GitLab and returns the token and some user info.
+   *
+   * @param {string} code - The request code.
+   * @returns {object} - An object with properties accessToken (string) and user (object { id (string), email (string), groups (string[])})
+   */
+  async requestAccessToken (code) {
+    const response = await fetch(`https://gitlab.lnu.se/oauth/token?client_id=${process.env.APP_ID}&client_secret=${process.env.APP_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=${process.env.CALLBACK}`, { method: 'POST' })
+
+    if (!response.ok) {
+      throw createError(response.status, response.statusText)
+    }
+
+    const { access_token: accessToken, id_token: idToken } = await response.json()
+    const payload = jwt.decode(idToken, { json: true })
+
+    return {
+      accessToken,
+      user: {
+        id: payload.sub,
+        email: payload.email,
+        groups: payload.groups_direct
+      }
+    }
+  }
 }
