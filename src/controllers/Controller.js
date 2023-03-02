9aaca3386461ccbec6aaa4df8d5faa8f9f5b4cad
Ellinor Henriksson <eh224kr@student.lnu.se>
2023-03-02 12:57:47 +0100

    Complete OAuth flow

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,7 +1,9 @@
 import { GitLabService } from '../services/GitLabService.js'
 import { ServiceBase } from '../services/ServiceBase.js'
-import { randomstring } from 'randomstring'
+import randomstring from 'randomstring'
 import fetch from 'node-fetch'
+import createError from 'http-errors'
+import jwt from 'jsonwebtoken'
 
 export class Controller {
   /**
@@ -13,53 +15,74 @@ export class Controller {
     this.#service = service
   }
 
-  index (req, res) {
+  index (req, res, next) {
     res.render('index')
   }
 
-  login (req, res) {
+  login (req, res, next) {
     const state = randomstring.generate()
     req.session.state = state
-    res.redirect(`https://gitlab.example.com/oauth/authorize?client_id=${process.env.APP_ID}&redirect_uri=${process.env.CALLBACK}&response_type=code&state=${state}&scope=read_api+openid`)
-    // Log in with OAuth
-    // Regenerate session
-    // Add user to req.session.user
+    res.redirect(307, `https://gitlab.lnu.se/oauth/authorize?client_id=${process.env.APP_ID}&redirect_uri=${process.env.CALLBACK}&response_type=code&state=${state}&scope=read_api+openid+profile+email`)
   }
 
-  profile (req, res) {
+  profile (req, res, next) {
     // Fetch profile info
     res.render('profile')
   }
 
-  activities (req, res) {
+  activities (req, res, next) {
     // Fetch activities
     res.render('activities')
   }
 
-  groups (req, res) {
+  groups (req, res, next) {
     // Fetch groups and projects
     res.render('groups')
   }
 
-  logout (req, res) {
+  logout (req, res, next) {
     // Log out with OAuth
     // Destroy session
   }
 
-  async oauthCallback (req, res) {
-    // Redirect url for oauth
+  // Redirect url for oauth
+  async oauthCallback (req, res, next) {
     if (req.query.state === req.session.state) {
-      // Proceed
       const code = req.query.code
+      console.log('Code: ', code)
       // Request access token
-      // OBS! Fortsättt här
-      const response = await fetch('')
+      try {
+        const response = await fetch(`https://gitlab.lnu.se/oauth/token?client_id=${process.env.APP_ID}&client_secret=${process.env.APP_SECRET}&code=${code}&grant_type=authorization_code&redirect_uri=${process.env.CALLBACK}`, { method: 'POST' })
 
-      // Check if response OK
+        if (!response.ok) {
+          console.log(response)
+          throw createError(response.status, response.statusText)
+        }
 
-      const data = await response.json()
+        const data = await response.json()
+
+        // Regenerate session
+        req.session.regenerate(error => {
+          if (error) {
+            throw new Error('Failed to regenerate session.')
+          }
+        })
+
+        // Store access token and user info in session store
+        req.session.accessToken = data.access_token
+        const payload = jwt.decode(data.id_token, { json: true })
+        req.session.user = {
+          id: payload.sub,
+          email: payload.email,
+          groups: payload.groups_direct
+        }
+
+        res.redirect('/')
+      } catch (error) {
+        next(error)
+      }
     } else {
-      // Error
+      next(createError(400))
     }
   }
 }
